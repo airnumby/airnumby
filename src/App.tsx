69466ee16c592bfe16b58e0edf147cd6a226eeb9
Pavel Schoffer <xschof00@gmail.com>
2021-10-03 01:57:36 +0200

    githubActions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,12 +10,12 @@ import './App.scss';
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route>
-        <DashboardPage />
-      </Route>
-    </Switch>
-  </Router>
+      <Switch>
+        <Route>
+          <DashboardPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
