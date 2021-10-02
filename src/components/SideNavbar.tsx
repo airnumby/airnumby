14e93b40b7179d5600fc0c98192d7f775373603e
Pavel Schoffer <xschof00@gmail.com>
2021-10-03 01:40:44 +0200

    sync colors

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -8,7 +8,7 @@ export default function SideNavbar() {
     console.log(window.location)
 
     return (
-        <div className="d-flex flex-column navbar navbar-light bg-light h-100" style={{ width: '250px' }}>
+        <div className="d-flex flex-column navbar navbar-dark bg-primary h-100" style={{ width: '250px' }}>
 
             <Link to='/'><div className="navbar-brand mb-3 me-0">{text.brand}</div></Link>
             <div className="flex-1 d-flex flex-column">
