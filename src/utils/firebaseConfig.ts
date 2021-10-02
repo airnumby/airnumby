c7481bf71db2d84fbfb1e6dfae3139e2eb3a57fd
Pavel Schoffer <xschof00@gmail.com>
2021-10-03 01:23:07 +0200

    correctConfig

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -2,13 +2,12 @@ import config from './config';
 
 
 const firebaseDevConfig = {
-    apiKey: "AIzaSyAvuPU4hUaNKTlEeb98dzp8_NmVBVmu2UE",
-    authDomain: "johan-friends-dev.firebaseapp.com",
-    projectId: "johan-friends-dev",
-    storageBucket: "johan-friends-dev.appspot.com",
-    messagingSenderId: "784244381293",
-    appId: "1:784244381293:web:2aa8612295595f91d95164",
-    measurementId: "G-SM1LN62RM3"
+    apiKey: "AIzaSyDWBolSbGC2KZoKm9S7Zt65AfAp3Iaa9bI",
+    authDomain: "airnumby.firebaseapp.com",
+    projectId: "airnumby",
+    storageBucket: "airnumby.appspot.com",
+    messagingSenderId: "482712424964",
+    appId: "1:482712424964:web:2ec2d6ba5d8c63fdb2db51"
 };
 
 const firebaseProdConfig = firebaseDevConfig;
