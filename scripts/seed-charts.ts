e491dc8f59b8b43a02282d64762a71b9d4ad0597
Pavel Schoffer <xschof00@gmail.com>
2021-10-14 19:50:48 +0200

    use firebase-admin for seed script

--- scripts/seed-charts.ts
+++ scripts/seed-charts.ts
@@ -1,27 +1,43 @@
 import fs from 'fs';
-import { execSync } from 'child_process';
-import { setDoc, doc, getFirestore  } from '@firebase/firestore';
-// noinspection ES6UnusedImports: Used by the eval below
-import * as firebase from '@firebase/app';
+import admin from 'firebase-admin';
+import { program } from 'commander';
+import { exit } from 'process';
 
-// This will run firebase.initializeApp({ ... })
-eval(execSync('firebase apps:sdkconfig', {encoding: 'utf8'}))
+program
+    .version('0.0.1')
+    .description('Seeds firestore with charts of accounts using admin SDK')
+    .parse(process.argv)
+
+const keyPath = `${__dirname}/admin-key.json`;
+if (!fs.existsSync(keyPath)) {
+    console.error(`Admin key not found in path ${keyPath}`);
+    exit(1);
+}
+
+admin.initializeApp({
+    credential: admin.credential.cert(keyPath)
+});
 
 const seedChart = async (chartId: string, name: string) => {
     const csvData = fs.readFileSync(`./assets/charts/${chartId}.csv`, 'utf8');
 
     const rows = csvData.split('\n')
-    const header = rows.pop()?.split(',')
+    const header = rows.shift()?.split(',');
 
     console.assert(header?.[0] === 'AccountNumber', `Invalid first column name "${header?.[0]}"`)
     console.assert(header?.[1] === 'AccountName', `Invalid second column name "${header?.[0]}"`)
 
-    const accounts = Object.fromEntries(rows.map(row => row.split(',')));
+    const accounts = Object.fromEntries(rows.map(row => {
+        const [key, name] = row.split(',')
+        const value = { name };
+        return [key, value];
+    }));
 
-    await setDoc(doc(getFirestore(), "charts", chartId), {
+    await admin.firestore().doc(`charts/${chartId}`).set({
         name,
         accounts,
     });
+    console.log(`Success! Chart ${chartId} uploaded.`)
 }
 
 (async () => {
